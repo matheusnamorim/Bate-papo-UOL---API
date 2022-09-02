4c959ae3729c1aea58ce38fceaddcdfe9d7309f3
Amorim <matheusnavesamorim@gmail.com>
2022-09-02 17:23:49 -0300

    Atualizando Status

--- src/app.js
+++ src/app.js
@@ -14,11 +14,17 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect(() => {
-    db = mongoClient.db('BateBapoUOL');
+    db = mongoClient.db('BatePapoUOL');
 });
 
 const userSchema = joi.object({
-    name: joi.string().min(1).required()
+    name: joi.string().trim().required()
+});
+
+const userSchema2 = joi.object({
+    to: joi.string().trim().required(),
+    text: joi.string().trim().required(),
+    type: joi.string().valid('message', 'private_message').required()
 });
 
 server.get('/participants', async (req,res) => {
@@ -38,8 +44,7 @@ server.get('/participants', async (req,res) => {
 server.post('/participants', async (req, res) => {
 
     let arrayParticipants = [];
-    const body = req.body;
-    const validation = userSchema.validate(body);
+    const validation = userSchema.validate(req.body);
 
     if(validation.error) {
         const message = validation.error.details.map(value => value.message);
@@ -72,17 +77,74 @@ server.post('/participants', async (req, res) => {
 });
 
 server.get('/messages', async (req, res) => {
+
+    const { user } = req.headers;
     const { limit } = req.query;
     try {
         const arrayMessages = await db.collection('messages').find().toArray();
         arrayMessages.map(value => delete value._id);
 
+        const messagesFiltered = arrayMessages.filter(value => {
+            if(value.type === 'private_message' && (value.from === user || value.to === user || value.to === 'Todos') || 
+            value.type === 'message' || value.type === 'status')
+                return value;
+        });
         if(Number.isInteger(Number(limit))){
-          let tam = arrayMessages.length - limit;
+          let tam = messagesFiltered.length - limit;
           if(tam < 0) tam = 0;
-          res.send(arrayMessages.filter((value, index) => {if(index >= tam) return value;}));  
+          res.send(messagesFiltered.filter((value, index) => {
+            if(index >= tam) {
+                return value;
+            }
+        }));  
         }else 
-            res.send(arrayMessages);
+            res.send(messagesFiltered);
+    } catch (error) {
+        res.status(500).send(error.message);
+        return;
+    }
+});
+
+server.post('/messages', async (req, res) => {
+    
+    const { user } = req.headers;
+    const validation = userSchema2.validate(req.body, { abortEarly: false });
+
+    if(validation.error){
+        const message = validation.error.details.map(value => value.message);
+        res.status(422).send(message);
+        return;
+    }
+    try {
+        const arrayParticipants = await db.collection('participants').find().toArray();
+        if(arrayParticipants.find(value => value.name === user) === undefined) {
+            res.sendStatus(422);
+            return;
+        }
+        db.collection('messages').insertOne({
+            from: user, 
+            ...req.body,
+            time: dayjs().format('HH:mm:ss')
+        });
+        res.sendStatus(201);
+        return;
+    } catch (error) {
+        res.status(500).send(error.message);
+        return;
+    }
+});
+
+server.post('/status', async (req, res) => {
+    const { user } = req.headers;
+    try {
+        const arrayParticipants = await db.collection('participants').find().toArray();
+        if(arrayParticipants.find(value => value.name === user) === undefined){
+            res.sendStatus(404);
+            return;
+        }
+        db.collection('participants').updateOne({name: user}, {$set: {lastStatus: Date.now()}});
+        res.sendStatus(200);
+        return;
     } catch (error) {
         res.status(500).send(error.message);
         return;
