395f139b9cc20b35e614e6074e5198363823cfd9
Amorim <matheusnavesamorim@gmail.com>
2022-09-02 10:29:43 -0300

    Conectando o banco e testando get

--- src/app.js
+++ src/app.js
@@ -8,5 +8,17 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI); 
+let db;
+
+mongoClient.connect(() => {
+    db = mongoClient.db('BateBapoUOL');
+});
+
+server.get('/participants', (req,res) => {
+    db.collection('participants').find().toArray().then(data => {
+        res.send(data);
+    });
+});
 
 server.listen(5000, () => console.log('Listening on port 5000'));
